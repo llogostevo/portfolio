8d51831a1c6b2d67379928958501e28b45f98d2a
Lloyd <stevens.lloyd@Gmail.com>
2023-05-07 12:30:06 +0000

    form email functionality

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+// Email modules
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 // import express framework
 const express = require('express');
 // import path module to work with file paths
@@ -11,12 +16,19 @@ const SKILLS = require('./skills');
 // create a new instance of the express application
 const app = express();
 
+// configure app to use bodyparser middleware to take from form to email 
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// express
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Configure mustache
 app.set('views', `${__dirname}/pages`);
 app.set('view engine', 'mustache');
 app.engine('mustache', mustacheExpress());
+
+
 
 // Render the template
 app.get('/', (req, res) => {
@@ -30,6 +42,39 @@ app.get('/', (req, res) => {
 app.get('/contactme', (req, res) => {
     res.render('contactme', {});
 })
+
+const transporter = nodemailer.createTransport({
+    host:'mail.gmx.com', //smtp host
+    port: 587, //smtp port
+    secure: false, // secure version of transport
+    auth: {
+        // user+pass keys from .env file
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+app.post('/contactme', (req,res) =>{
+    console.log(req.body);
+    const {name, email, inquiryType, message} = req.body;
+    const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: process.env.EMAIL_ID,
+        subject: `New inquiry from ${name} - ${inquiryType}`,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info)=> {
+        if (error) {
+            console.error(error);
+            res.send('There was an error sending your message, please try again')
+        } else {
+            console.log('Email sent: '+info.response);
+            res.status(200).send('Your message was sent successfully!');
+        }
+    });
+
+});
 
 // set the port the server will listen to
 const port = process.env.PORT || 3000;
